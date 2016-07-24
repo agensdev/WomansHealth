0ac8e65e9a10aa4665923ffb91439861fee2b9a5
Blanka Kulik <kulik.blanka@gmail.com>
2016-07-24 17:30:24 +0200

    Add changes to medications activity

--- app/src/main/java/com/blashca/womanshealth/MedicationsActivity.java
+++ app/src/main/java/com/blashca/womanshealth/MedicationsActivity.java
@@ -2,37 +2,71 @@ package com.blashca.womanshealth;
 
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 
+import com.blashca.womanshealth.data.WomansHealthContract;
+import com.blashca.womanshealth.data.WomansHealthDbHelper;
+
 public class MedicationsActivity extends AppCompatActivity {
-    String[] medicationsNameList;
-    private static String ID = "myIndex";
+    private WomansHealthDbHelper dbHelper;
+    private static String MEDICATION_ID = "medicationId";
+    private CursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medications);
 
+        dbHelper = new WomansHealthDbHelper(this);
+
         ListView listView = (ListView) findViewById(R.id.medications_listView);
-
-        medicationsNameList = new String[] {"Medicine1", "Medicine2", "Medicine3", "Medicine4","Medicine5","Medicine6",
-                "Medicine7", "Medicine8", "Medicine9", "Medicine10", "Medicine11", "Medicine12"};
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.medications_list_item, medicationsNameList);
-        listView.setAdapter(arrayAdapter);
+        mAdapter = new MedicationCursorAdapter(this, dbHelper.getMedicationsCursor(), 0);
+        listView.setAdapter(mAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MedicationsActivity.this, MedicationsDetailActivity.class);
-                intent.putExtra(ID, id);
-                startActivityForResult(intent, 1);
+                Cursor cursor = dbHelper.getMedicationIdCursor(id);
+                int isAllergen = cursor.getInt(cursor.getColumnIndex(WomansHealthContract.WomansHealthMedication.COLUMN_IS_ALLERGEN));
+
+                if (isAllergen != 1) {
+                    Intent intent = new Intent(getApplicationContext(), MedicationDetailActivity.class);
+                    intent.putExtra(MEDICATION_ID, id);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), AllergenDetailActivity.class);
+                    intent.putExtra(MEDICATION_ID, id);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    public void onAddAllergyButtonClicked(View view) {
+        Intent intent = new Intent(getApplicationContext(), AllergenEditActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAddMedicationButtonClicked(View view) {
+        Intent intent = new Intent(getApplicationContext(), MedicationEditActivity.class);
+        startActivity(intent);
+    }
+
+    private void refresh() {
+        mAdapter.changeCursor(dbHelper.getMedicationsCursor());
+        // notifies adapter on the change
+        mAdapter.notifyDataSetChanged();
     }
 }
