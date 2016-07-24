351cfe7a1a75bad1da648ceae0d5fcf24e1a5d24
Blanka Kulik <kulik.blanka@gmail.com>
2016-07-24 17:31:45 +0200

    Add changes to appointments activity

--- app/src/main/java/com/blashca/womanshealth/AppointmentsActivity.java
+++ app/src/main/java/com/blashca/womanshealth/AppointmentsActivity.java
@@ -6,33 +6,51 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 
+import com.blashca.womanshealth.data.WomansHealthDbHelper;
+
 public class AppointmentsActivity extends AppCompatActivity {
-    String[] appointmentsList;
-    private static String ID = "myIndex";
+    private WomansHealthDbHelper dbHelper;
+    private static String APPOINTMENT_ID = "appointmentId";
+    private CursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointments);
 
+        dbHelper = new WomansHealthDbHelper(this);
+
         ListView listView = (ListView) findViewById(R.id.appointments_listView);
-
-        appointmentsList = new String[] {"Appointment1", "Appointment2", "Appointment3", "Appointment4","Appointment5","Appointment6",
-                "Appointment7", "Appointment8", "Appointment9", "Appointment10", "Appointment11", "Appointment12"};
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.appointments_list_item, appointmentsList);
-        listView.setAdapter(arrayAdapter);
+        mAdapter = new AppointmentCursorAdapter(this, dbHelper.getAppointmentsCursor(), 0);
+        listView.setAdapter(mAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(AppointmentsActivity.this, AppointmentsDetailActivity.class);
-                intent.putExtra(ID, id);
-                startActivityForResult(intent, 1);
+                Intent intent = new Intent(getApplicationContext(), AppointmentDetailActivity.class);
+                intent.putExtra(APPOINTMENT_ID, id);
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    public void onAddAppointmentButtonClicked(View view) {
+        Intent intent = new Intent(getApplicationContext(), AppointmentEditActivity.class);
+        startActivity(intent);
+    }
+
+    private void refresh() {
+        mAdapter.changeCursor(dbHelper.getAppointmentsCursor());
+        // notifies adapter on the change
+        mAdapter.notifyDataSetChanged();
     }
 }
