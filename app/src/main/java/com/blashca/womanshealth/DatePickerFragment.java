86eb0965117b157f9b4176f6e6c0d6dd082b3253
Blanka Kulik <kulik.blanka@gmail.com>
2016-07-24 17:25:12 +0200

    Add date picker fragment

--- app/src/main/java/com/blashca/womanshealth/DatePickerFragment.java
+++ app/src/main/java/com/blashca/womanshealth/DatePickerFragment.java
@@ -1,14 +1,31 @@
 package com.blashca.womanshealth;
 
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
-public class DatePickerFragment extends DialogFragment {
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    private TextView textView;
+    private DateFormat dateFormat;
+
+    public DatePickerFragment() {
+
+    }
+
+    @SuppressLint("ValidFragment")
+    public DatePickerFragment(TextView textView) {
+        this.textView = textView;
+        dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -19,6 +36,16 @@ public class DatePickerFragment extends DialogFragment {
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, monthOfYear, dayOfMonth);
+        Date date = calendar.getTime();
+        String formattedDate = dateFormat.format(date);
+
+        textView.setText(formattedDate);
     }
 }
