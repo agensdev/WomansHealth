6513e2d22ea30ad3b27ba09f9057ca1dedd743d3
Blanka Kulik <kulik.blanka@gmail.com>
2016-08-19 19:52:53 -0400

    Add bmi calculation class

---
+++ app/src/main/java/com/blashca/womanshealth/Bmi.java
@@ -0,0 +1,60 @@
+package com.blashca.womanshealth;
+
+
+public class Bmi {
+    private double height;
+    private double weight;
+    private double bmi;
+
+    public Bmi(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+        bmi = weight / Math.pow(height/100.0, 2);
+    }
+
+    public String getBmiValue() {
+        return String.format("%.1f", bmi);
+    }
+
+    public int getCategory() {
+        int category;
+
+        if (bmi < 18.5) {
+            category = R.string.underweight;
+        } else if (bmi >= 18.5 && bmi < 25) {
+            category = R.string.normal_weight;
+        } else if (bmi >= 25 && bmi < 30) {
+            category = R.string.overweight;
+        } else {
+            category = R.string.obese;
+        }
+
+        return category;
+    }
+
+    public int getOptimum() {
+        int optimum;
+
+        if (bmi < 18.5) {
+            optimum = R.string.underweight_result;
+        } else if (bmi >= 18.5 && bmi < 25) {
+            optimum = R.string.normal_result;
+        } else {
+            optimum = R.string.overweight_result;
+        }
+
+        return optimum;
+    }
+
+    public String getOptimalWeight() {
+        double optimalWeight;
+
+        if (bmi < 18.5) {
+            optimalWeight = Math.abs(weight - (18.5 * Math.pow(height/100.0, 2)));
+        } else {
+            optimalWeight = Math.abs((24.9 * Math.pow(height/100.0, 2)) - weight);
+        }
+
+        return String.format("%.1f", optimalWeight);
+    }
+}
