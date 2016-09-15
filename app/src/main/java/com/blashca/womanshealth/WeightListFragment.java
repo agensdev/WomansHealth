948277b4413960c2c24fd357f20d80fa10c5629f
Blanka Kulik <kulik.blanka@gmail.com>
2016-09-15 17:22:11 +0200

    Add weight records activity

--- app/src/main/java/com/blashca/womanshealth/WeightListFragment.java
+++ app/src/main/java/com/blashca/womanshealth/WeightListFragment.java
@@ -3,36 +3,45 @@ package com.blashca.womanshealth;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 
 import com.blashca.womanshealth.data.WomansHealthDbHelper;
 
-public class WeightRecordsActivity extends AppCompatActivity {
+public class WeightListFragment extends Fragment {
     private WomansHealthDbHelper dbHelper;
-    private CursorAdapter mAdapter;
+    private CursorAdapter myCursorAdapter;
     private long weightId;
 
+
+    /**
+     * Create a new instance of WeightListFragment
+     */
+    public static WeightListFragment newInstance() {
+        return new WeightListFragment();
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weight_records);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weight_records_list, container, false);
+        dbHelper = new WomansHealthDbHelper(getContext());
 
-        dbHelper = new WomansHealthDbHelper(this);
-
-        ListView listView = (ListView) findViewById(R.id.weights_listView);
-        mAdapter = new WeightCursorAdapter(this, dbHelper.getWeightsCursor(), 0);
-        listView.setAdapter(mAdapter);
+        ListView listView = (ListView) view.findViewById(R.id.weights_listView);
+        myCursorAdapter = new WeightCursorAdapter(getContext(), dbHelper.getWeightsCursor(), 0);
+        listView.setAdapter(myCursorAdapter);
 
         listView.setOnLongClickListener(new AdapterView.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 weightId = v.getId();
 
-                android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(getApplicationContext());
+                android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(getContext());
 
                 alertDialogBuilder
                         .setMessage(R.string.delete_record)
@@ -40,7 +49,7 @@ public class WeightRecordsActivity extends AppCompatActivity {
                         .setPositiveButton(R.string.delete,new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 dbHelper.deleteWeight(weightId);
-                                finish();
+                                //finish();
                             }
                         })
                         .setNegativeButton(R.string.cancel,new DialogInterface.OnClickListener() {
@@ -56,17 +65,18 @@ public class WeightRecordsActivity extends AppCompatActivity {
                 return true;
             }
         });
+        return view;
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         refresh();
     }
 
     private void refresh() {
-        mAdapter.changeCursor(dbHelper.getWeightsCursor());
+        myCursorAdapter.changeCursor(dbHelper.getWeightsCursor());
         // notifies adapter on the change
-        mAdapter.notifyDataSetChanged();
+        myCursorAdapter.notifyDataSetChanged();
     }
 }
