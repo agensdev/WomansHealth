507dd9d7e1f04bf1bd290a2b960d4b07eabc9890
Blanka Kulik <kulik.blanka@gmail.com>
2016-10-04 17:26:22 +0200

    Separate MyPagerAdapter class from WeightRecordsActivity

--- app/src/main/java/com/blashca/womanshealth/MyPagerAdapter.java
+++ app/src/main/java/com/blashca/womanshealth/MyPagerAdapter.java
@@ -1,8 +1,49 @@
 package com.blashca.womanshealth;
 
-/**
- * Created by blanka on 03.10.2016.
- */
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
-public class MyPagerAdapter {
+
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    private Fragment weightList;
+    private Fragment weightChart;
+    private static final int NUM_ITEMS = 2;
+    private String listTitle;
+    private String chartTitle;
+
+    public MyPagerAdapter(FragmentManager fm, Fragment fragment1, Fragment fragment2, String listTitle, String chartTitle) {
+        super(fm);
+        weightList = fragment1;
+        weightChart = fragment2;
+        this.listTitle = listTitle;
+        this.chartTitle = chartTitle;
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        if (position == 0) {
+            return listTitle;
+        } else if (position == 1) {
+            return chartTitle;
+        }
+
+        return "";
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return weightList;
+        } else {
+            return weightChart;
+        }
+    }
 }
--- app/src/main/java/com/blashca/womanshealth/WeightRecordsActivity.java
+++ app/src/main/java/com/blashca/womanshealth/WeightRecordsActivity.java
@@ -1,55 +1,41 @@
 package com.blashca.womanshealth;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import com.blashca.womanshealth.external.SlidingTabLayout;
 
 
 public class WeightRecordsActivity extends FragmentActivity {
-    static final int NUM_ITEMS = 2;
+    private WeightListFragment weightList;
+    private WeightChartFragment weightChart;
 
-    private ViewPager mViewPager;
-    private SlidingTabLayout mSlidingTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_records);
 
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        //We're creating instances of two fragments
+        weightList = WeightListFragment.newInstance();
+        weightChart = WeightChartFragment.newInstance();
 
-        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
-        mSlidingTabLayout.setViewPager(mViewPager);
+        String listTitle = getResources().getString(R.string.list);
+        String chartTitle = getResources().getString(R.string.chart);
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        //We're passing two fragment instances to MyAdapter
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), weightList, weightChart, listTitle, chartTitle));
+
+        SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setViewPager(mViewPager);
     }
 
-    public static class MyAdapter extends FragmentPagerAdapter {
-        public MyAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_ITEMS;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Item" + (position + 1); //super.getPageTitle(position);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            if (position == 0) {
-                return WeightListFragment.newInstance();
-            } else {
-                return WeightChartFragment.newInstance();
-            }
-        }
+    public void onDataChange() {
+        weightList.refresh();
+        weightChart.refreshChart();
     }
 }
