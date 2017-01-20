84f060be460e30b94050386b4380a303572bc710
Blanka Kulik <kulik.blanka@gmail.com>
2017-01-20 15:47:15 +0100

    Add password digest

---
+++ app/src/main/java/com/blashca/womanshealth/DigestUtil.java
@@ -0,0 +1,43 @@
+package com.blashca.womanshealth;
+
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class DigestUtil {
+
+    public static String digestPassword(String passwordText) {
+
+        String hash = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] bytes = passwordText.getBytes("UTF-8");
+            digest.update(bytes, 0, bytes.length);
+            bytes = digest.digest();
+
+            // This is ~55x faster than looping and String.formating()
+            hash = bytesToHex(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch( UnsupportedEncodingException e ) {
+            e.printStackTrace();
+        }
+
+        return hash;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+
+        char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+        char[] hexChars = new char[ bytes.length * 2 ];
+        for(int j = 0; j < bytes.length; j++)
+        {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
