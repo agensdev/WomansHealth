9f026aeaab8ea9d90b5f1a425820009d0f605598
Blanka Kulik <kulik.blanka@gmail.com>
2017-04-24 11:30:31 +0200

    Add header to suggested appointments scrollview

--- app/src/main/java/com/blashca/womanshealth/activities/SuggestedAppointmentsActivity.java
+++ app/src/main/java/com/blashca/womanshealth/activities/SuggestedAppointmentsActivity.java
@@ -5,7 +5,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
@@ -37,6 +39,11 @@ public class SuggestedAppointmentsActivity extends AppCompatActivity {
         dbHelper = new WomansHealthDbHelper(this);
 
         ListView listView = (ListView) findViewById(R.id.suggested_appointments_listView);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup) layoutInflater.inflate(R.layout.suggested_apointments_list_header, listView, false);
+        listView.addHeaderView(header);
+
         mAdapter = new SuggestedAppointmentCursorAdapter(this, dbHelper.getMedicalTestsCursorByAge(getAge()), 0);
         listView.setAdapter(mAdapter);
 
